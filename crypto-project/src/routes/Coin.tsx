ffbd54de71f3a9fa4daa014c6be69db2885b654c
hana cho <hanacho@hanaui-MacBookPro.local>
2025-03-23 03:16:47 +0900

    price, chart 페이지 nested router 추가

--- crypto-project/src/routes/Coin.tsx
+++ crypto-project/src/routes/Coin.tsx
@@ -1,23 +1,32 @@
-import { useParams, useLocation } from "react-router-dom";
+import {
+  useParams,
+  useLocation,
+  Route,
+  Routes,
+  Outlet,
+  Link,
+} from "react-router-dom";
 import styled from "styled-components";
 import { useState, useEffect } from "react";
+import Chart from "./Chart";
+import Price from "./Price";
 
 const Contianer = styled.div`
-padding: 0px 20px;
+  padding: 0px 20px;
 `;
 
 const Header = styled.header`
-   height: 15vh;
-   width: 100%;
-   display: flex; 
-   justify-content: center;
-   align-items: center;
- `;
+  height: 15vh;
+  width: 100%;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+`;
 
 const Title = styled.h1`
-color: ${(props) => props.theme.accentColor};
-font-size: 48px;
-`
+  color: ${(props) => props.theme.accentColor};
+  font-size: 48px;
+`;
 
 const Loader = styled.div`
   margin-top: 20px;
@@ -25,6 +34,29 @@ const Loader = styled.div`
   display: flex;
   justify-content: center;
   font-size: 24px;
+`;
+
+const Overview = styled.div`
+  display: flex;
+  justify-content: space-between;
+  background-color: rgba(0, 0, 0, 0.5);
+  padding: 10px 20px;
+  border-radius: 10px;
+`;
+const OverviewItem = styled.div`
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+
+  span:first-child {
+    font-size: 10px;
+    font-weight: 400;
+    text-transform: uppercase;
+    margin-bottom: 5px;
+  }
+`;
+const Description = styled.p`
+  margin: 20px 0px;
 `;
 
 interface CoinParams {
@@ -53,8 +85,6 @@ interface CoinInfo {
   last_data_at: string;
 }
 
-
-
 interface CoinPrice {
   id: string;
   name: string;
@@ -66,55 +96,96 @@ interface CoinPrice {
   first_data_at: string;
   last_updated: string;
   quotes: {
-    USD : {
+    USD: {
       ath_date: string;
-      ath_price :number;
-      market_cap :number;
-      market_cap_change_24h :number;
-      percent_change_1h :number;
-      percent_change_1y :number;
-      percent_change_6h :number;
-      percent_change_7d :number;
-      percent_change_12h :number;
-      percent_change_15m :number;
-      percent_change_24h :number;
-      percent_change_30d :number;
-      percent_change_30m :number;
-      percent_from_price_ath :number;
-      price :number;
-      volume_24h :number;
-      volume_24h_change_24h :number;
-    }
+      ath_price: number;
+      market_cap: number;
+      market_cap_change_24h: number;
+      percent_change_1h: number;
+      percent_change_1y: number;
+      percent_change_6h: number;
+      percent_change_7d: number;
+      percent_change_12h: number;
+      percent_change_15m: number;
+      percent_change_24h: number;
+      percent_change_30d: number;
+      percent_change_30m: number;
+      percent_from_price_ath: number;
+      price: number;
+      volume_24h: number;
+      volume_24h_change_24h: number;
+    };
   };
 }
 
 function Coin() {
-
   const [loading, setLoading] = useState(true);
   const { coinId } = useParams();
   const location = useLocation();
   const state = location.state as CoinParams;
   const [info, setInfo] = useState<CoinInfo | undefined>();
   const [price, setPrice] = useState<CoinPrice | undefined>();
-  
+
   useEffect(() => {
-    ( async () => { 
-      const coinInfo = await (await fetch(`https://api.coinpaprika.com/v1/coins/${coinId}`)).json();
-      const priceData = await (await fetch(`https://api.coinpaprika.com/v1/tickers/${coinId}`)).json();
+    (async () => {
+      const coinInfo = await (
+        await fetch(`https://api.coinpaprika.com/v1/coins/${coinId}`)
+      ).json();
+      const priceData = await (
+        await fetch(`https://api.coinpaprika.com/v1/tickers/${coinId}`)
+      ).json();
       console.log(coinInfo);
       console.log(priceData);
       setLoading(false);
       setInfo(coinInfo as CoinInfo);
       setPrice(priceData as CoinPrice);
-    } )()
-  }, []);
+    })();
+  }, [coinId]);
 
   return (
     <Contianer>
       <Header>
-        <Title>{state?.name || "Invalid"}</Title>
-        </Header>
-        {loading ? <Loader>Loading...</Loader> :  <span>{info ? info.description : null} {price ? price.quotes.USD.ath_price : null}</span>}
+        <Title>
+          {state?.name ? state.name : loading ? "Loding.." : info?.name}
+        </Title>
+      </Header>
+      {loading ? (
+        <Loader>Loading...</Loader>
+      ) : (
+        <>
+          <Overview>
+            <OverviewItem>
+              <span>Rank:</span>
+              <span>{info?.rank}</span>
+            </OverviewItem>
+            <OverviewItem>
+              <span>Symbol:</span>
+              <span>${info?.symbol}</span>
+            </OverviewItem>
+            <OverviewItem>
+              <span>Open Source:</span>
+              <span>{info?.open_source ? "Yes" : "No"}</span>
+            </OverviewItem>
+          </Overview>
+          <Description>{info?.description}</Description>
+          <Overview>
+            <OverviewItem>
+              <span>Total Suply:</span>
+              <span>{price?.total_supply}</span>
+            </OverviewItem>
+            <OverviewItem>
+              <span>Max Supply:</span>
+              <span>{price?.max_supply}</span>
+            </OverviewItem>
+          </Overview>
+
+          <nav>
+            <Link to="price">Price</Link>
+            <Link to="chart">Chart</Link>
+          </nav>
+          <Outlet />
+        </>
+      )}
     </Contianer>
   );
 }
