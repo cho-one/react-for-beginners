39a215ed240420867fff12ddff958191488eb7e8
hana cho <hanacho@hanaui-MacBookPro.local>
2025-03-23 00:25:02 +0900

    coins page 디자인 작업 및 임시 코인 아이템 생성

--- crypto-project/src/App.tsx
+++ crypto-project/src/App.tsx
@@ -69,6 +69,7 @@ table {
 
   a {
     text-decoration: none;
+    color: inherit;
     }
 `
 
--- crypto-project/src/routes/Coins.tsx
+++ crypto-project/src/routes/Coins.tsx
@@ -1,11 +1,71 @@
+import { Link } from "react-router-dom";
 import styled from "styled-components";
+
+const Contianer = styled.div`
+padding: 0px 20px;
+`;
+
+const Header = styled.header`
+   height: 15vh;
+   display: flex;
+   justify-content: center;
+   align-items: center;
+ `;
+
+const CoinsList = styled.ul``;
+
+const Coin = styled.li`
+   background-color: white;
+   color: ${(props) => props.theme.bgColor};
+   border-radius: 15px;
+   margin-bottom: 10px;
+   a {
+     padding: 20px;
+     transition: color 0.2s ease-in;
+     display: block;
+   }
+   &:hover {
+     a {
+       color: ${(props) => props.theme.accentColor};
+     }
+   }
+ `;
 
 const Title = styled.h1`
 color: ${(props) => props.theme.accentColor};
+font-size: 48px;
 `
 
+const coins = [
+  {
+    id: "bitcoin",
+    name: "Bitcoin",
+  },
+  {
+    id: "ethereum",
+    name: "Ethereum",
+  },
+  {
+    id: "ripple",
+    name: "Ripple",
+  },
+];
+ 
+
 function Coins() {
-  return <Title>코인</Title>;
+  return (
+    <Contianer>
+      <Header>
+        <Title>Coins</Title>
+        <CoinsList>
+          {coins.map((coin) => (
+            <Coin key={coin.id}>
+              <Link to={`${coin.id}`}>{coin.name} &rarr;</Link>
+          </Coin>) )}
+        </CoinsList>
+      </Header>
+    </Contianer>
+  );
 }
 
 export default Coins;
