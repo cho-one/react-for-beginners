e0e569cdb447bc073f604f0b15a40259888a7b40
hana cho <hanacho@hanaui-MacBookPro.local>
2025-03-23 01:04:34 +0900

    fetch api 호출

--- crypto-project/src/routes/Coins.tsx
+++ crypto-project/src/routes/Coins.tsx
@@ -1,5 +1,6 @@
 import { Link } from "react-router-dom";
 import styled from "styled-components";
+import { useState, useEffect } from "react";
 
 const Contianer = styled.div`
 padding: 0px 20px;
@@ -39,35 +40,50 @@ color: ${(props) => props.theme.accentColor};
 font-size: 48px;
 `
 
-const coins = [
-  {
-    id: "bitcoin",
-    name: "Bitcoin",
-  },
-  {
-    id: "ethereum",
-    name: "Ethereum",
-  },
-  {
-    id: "ripple",
-    name: "Ripple",
-  },
-];
- 
+const Loader = styled.div`
+  margin-top: 20px;
+  width: 100%;
+  display: flex;
+  justify-content: center;
+  font-size: 24px;
+`;
+
+interface CoinInterface{
+  id: string;
+  name: string;
+  symbol: string;
+  rank: number;
+  is_new: boolean;
+  is_active: boolean;
+  type: string;
+}
 
 function Coins() {
+  const [coins, setCoins] = useState<CoinInterface[]>([]);
+  const [loading, setLoading] = useState(true); 
+  useEffect(() => {
+    ( async () => { 
+      const response = await fetch("https://api.coinpaprika.com/v1/coins");
+      const json = await response.json();
+      console.log(json);
+      setCoins(json.slice(0, 100));
+      setLoading(false);
+    } )()
+  }, []);
+
   return (
     <Contianer>
       <Header>
         <Title>Coins</Title>
         </Header>
+        {loading ? <Loader>Loading...</Loader> : 
         <CoinsList>
           {coins.map((coin) => (
             <Coin key={coin.id}>
               <Link to={`${coin.id}`}>{coin.name} &rarr;</Link>
           </Coin>) )}
         </CoinsList>
-      
+        }
     </Contianer>
   );
 }
