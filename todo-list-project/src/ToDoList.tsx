9f2b6a5cf1571d642ace071e89bf25c680a72497
hana cho <hanacho@hanaui-MacBookPro.local>
2025-03-28 01:19:18 +0900

    react-hook-form

--- todo-list-project/src/ToDoList.tsx
+++ todo-list-project/src/ToDoList.tsx
@@ -1,51 +1,75 @@
-import { useState } from "react";
 import { useForm } from "react-hook-form";
 
-// function ToDoList() {
-//   const [toDo, setToDo] = useState("");
-//   const [toDoError, setToDoError] = useState("");
-//   const onChange = (event: React.FormEvent<HTMLInputElement>) => {
-//     const {
-//       currentTarget: { value },
-//     } = event;
-//     setToDoError("");
-//     setToDo(value);
-//   };
-
-//   const onSubmit = (event: React.FormEvent<HTMLFormElement>) => {
-//     event.preventDefault();
-//     if (toDo.length < 10) {
-//       return setToDoError("To do should be longer");
-//     }
-//     console.log("submit");
-//   };
-//   return (
-//     <div>
-//       <form onSubmit={onSubmit}>
-//         <input
-//           onChange={onChange}
-//           value={toDo}
-//           placeholder="Wirte a to do"
-//         ></input>
-//         <button>Add</button>
-//         {toDoError !== "" ? toDoError : null}
-//       </form>
-//     </div>
-//   );
-// }
+interface IForm {
+  email: string;
+  firstName: string;
+  lastName: string;
+  userName: string;
+  password: string;
+  password1: string;
+}
 
 function ToDoList() {
-  const { register, watch } = useForm();
-  console.log(watch());
+  const {
+    register,
+    handleSubmit,
+    formState: { errors },
+  } = useForm<IForm>({
+    defaultValues: {
+      email: "@naver.com",
+    },
+  });
+
+  const onValid = (data: any) => {
+    console.log(data);
+  };
+  //   console.log(formState.errors);
+  //   console.log(watch());
+
   return (
     <div>
-      <form>
-        <input {...register("email")} placeholder="Email"></input>
-        <input {...register("firstName")} placeholder="firstName"></input>
-        <input {...register("lastName")} placeholder="lastName"></input>
-        <input {...register("userName")} placeholder="userName"></input>
-        <input {...register("password")} placeholder="password"></input>
-        <input {...register("password1")} placeholder="password1"></input>
+      <form
+        style={{ display: "flex", flexDirection: "column" }}
+        onSubmit={handleSubmit(onValid)}
+      >
+        <input
+          {...register("email", {
+            required: "Email is required",
+            pattern: {
+              value: /^[A-Za-z0-9._%+-]+@naver.com$/,
+              message: "Only naver.com emails allowed",
+            },
+          })}
+          placeholder="Email"
+        ></input>
+        <span>{errors?.email?.message}</span>
+
+        <input
+          {...register("firstName", { required: "write here" })}
+          placeholder="First Name"
+        />
+        <span>{errors?.firstName?.message}</span>
+
+        <input
+          {...register("lastName", { required: "write here" })}
+          placeholder="lastName"
+        ></input>
+        <span>{errors?.lastName?.message}</span>
+        <input
+          {...register("userName", { required: "write here", minLength: 10 })}
+          placeholder="userName"
+        ></input>
+        <span>{errors?.userName?.message}</span>
+        <input
+          {...register("password", { required: "write here" })}
+          placeholder="password"
+        ></input>
+        <span>{errors?.password?.message}</span>
+        <input
+          {...register("password1", { required: "Password is required" })}
+          placeholder="password1"
+        ></input>
+        <span>{errors?.password1?.message}</span>
         <button>Add</button>
       </form>
     </div>
